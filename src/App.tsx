df2ce4e5069a537d348cf6404fd4702291763fa0
Polina Pavlenko <polina.pavvlenko@gmail.com>
2025-05-29 14:31:28 +0300

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Button colorPalette="blue">
-      Button
-    </Button>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem
+        area="aside"
+        bg="gold"
+        display={{
+          base: "none",
+          lg: "block",
+        }}
+      >
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
